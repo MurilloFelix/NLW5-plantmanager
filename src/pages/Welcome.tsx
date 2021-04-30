b91f2040e709412596072e66f44277a5ab139079
MurilloFelix <murillo.felix1511@gmail.com>
2021-04-30 13:02:04 -0300

    Wip Dia 2 Nlw

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet} from 'react-native' 
+import React, {useState} from 'react';
+import { SafeAreaView, Text, Image, StyleSheet, TouchableOpacity, Dimensions} from 'react-native' 
 
+import { Feather } from '@expo/vector-icons'
 import wateringImg from '../assets/watering.png'
 import colors from '../style/colors';
-import { Button } from '../components/Button' 
+import fonts from '../style/fonts'
 
 export function Welcome(){
     return(
@@ -14,14 +15,24 @@ export function Welcome(){
                 de forma fácil
             </Text>
 
-            <Image source={wateringImg} style={styles.image}/>
+            <Image 
+                source={wateringImg} 
+                style={styles.image}
+                resizeMode="contain"
+            />
 
             <Text style={styles.subtitle}>
                 Não esqueça mais de regar suas plantas. 
                 Nós cuidamos de lembrar você sempre que precisar. 
+                test
             </Text>
             
-            <Button title="Avançar"/>
+            <TouchableOpacity 
+                style={styles.button} 
+                activeOpacity={0.7}
+                >
+                <Feather name="chevron-right" style={styles.buttonIcon}/>
+            </TouchableOpacity>
 
         </SafeAreaView>
     )
@@ -31,23 +42,38 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around'
     },
     title: {
         fontSize: 32,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 38
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 38 
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text 
     },
     image: {
-        width: 292,
-        height: 284
+        height: Dimensions.get('window').width * 0.7      
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+    buttonIcon: {
+        fontSize: 32,
+        color: colors.white
     }
 })
